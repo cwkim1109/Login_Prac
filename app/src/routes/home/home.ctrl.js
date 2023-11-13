4f1fb23ee55ddb18141a6b525ddf5790086f87a9
bobby.kim <bobby.kim@ahha.ai>
2023-11-13 18:09:21 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,28 @@ const output = {
   },
 };
 
+const users = {
+  id: ["Bobby", "Sally", "Corn"],
+  pwd: ["1234", "1234", "1234"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      pwd = req.body.pwd
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pwd[idx] === pwd) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   }
 };
 
